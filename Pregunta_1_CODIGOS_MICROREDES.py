578683368c14947cec1ae4efa8efcac3a52e08a7
DiegoAriasCumican <105386647+DiegoAriasCumican@users.noreply.github.com>
2023-10-09 16:39:01 -0300

    Update and rename CODIGOS_MICROREDES.py to Pregunta_1_CODIGOS_MICROREDES.py

--- Pregunta_1_CODIGOS_MICROREDES.py
+++ Pregunta_1_CODIGOS_MICROREDES.py
@@ -1,3 +1,4 @@
+##################################################PREGUNTA1.1##############################################################
 import numpy as np
 import pandas as pd
 import pandapower as pp
